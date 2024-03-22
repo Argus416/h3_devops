f95a44654ef24ba796300417053b297de888d241
Mohamad AL KHATIB <alkhatib.m804@gmail.com>
2024-03-22 13:16:27 +0100

    Fix url

--- app/src/config/api_config.js
+++ app/src/config/api_config.js
@@ -1,3 +1,4 @@
-const URL = "http://mohamad-facebook-backend.default.svc.cluster.local";
+// const URL = "http://mohamad-facebook-backend.default.svc.cluster.local";
+const URL = 'http://4.225.193.68'
 export const API_URL = `${URL}/v1`;
 export const API_PUBLIC_URL = `${URL}`;
