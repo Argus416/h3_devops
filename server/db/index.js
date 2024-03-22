f32ee9e7db0ebac06d4bca328aa121596757e22a
Mohamad AL KHATIB <alkhatib.m804@gmail.com>
2024-03-22 10:47:02 +0100

    Update api_config

--- server/db/index.js
+++ server/db/index.js
@@ -3,8 +3,6 @@ const pg = require("knex")({
     client: "pg",
     connection: DB_CONNECTION_CONFIG,
     searchPath: ["knex", "public"],
-    ssl: { rejectUnauthorized: false }
-
 });
 
 module.exports = pg;
