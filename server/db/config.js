38fca35c0499f1b22c45a256740e061125714d21
Mohamad AL KHATIB <alkhatib.m804@gmail.com>
2024-03-22 10:33:40 +0100

    Update db

--- server/db/config.js
+++ server/db/config.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 
 const DB_CONNECTION_CONFIG = {
-	host: "postgres",
-	user: "mohamad",
-	password: "123321",
-	database: "postgres",
+	host: "dpg-cnuksle3e1ms73cp9ru0-a.frankfurt-postgres.render.com",
+	user: "mohamad_0yuy_user",
+	password: "2P8LALTCVXukhgE6ncsG8MlGkFzLBKV7",
+	database: "mohamad_0yuy",
 	port: 5432,
+	ssl: { rejectUnauthorized: false },
 };
 
 module.exports = DB_CONNECTION_CONFIG;
+
