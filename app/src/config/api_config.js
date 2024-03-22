df161c4d8fde8a4f9033159b08072ea5b6cb252e
Mohamad AL KHATIB <alkhatib.m804@gmail.com>
2024-03-22 11:26:07 +0100

    Add azure

--- app/src/config/api_config.js
+++ app/src/config/api_config.js
@@ -1,2 +1,3 @@
-export const API_URL = "http://mohamad-facebook-backend.default.svc.cluster.local:3001/v1";
-export const API_PUBLIC_URL = "http://mohamad-facebook-backend.default.svc.cluster.local:3001";
+const URL = "http://mohamad-facebook-backend.default.svc.cluster.local:3001";
+export const API_URL = `${URL}:3001/v1`;
+export const API_PUBLIC_URL = `${URL}:3001`;
