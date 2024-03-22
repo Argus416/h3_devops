bf0d04282962f90f225658b99a224fb30dc41e88
Mohamad AL KHATIB <alkhatib.m804@gmail.com>
2024-03-22 12:38:33 +0100

    Update

--- app/src/config/api_config.js
+++ app/src/config/api_config.js
@@ -1,3 +1,3 @@
-const URL = "http://mohamad-facebook-backend.default";
+const URL = "http://mohamad-facebook-backend.default.svc.cluster.local";
 export const API_URL = `${URL}/v1`;
 export const API_PUBLIC_URL = `${URL}`;
